9ec8d7f3492c61dcaf33413d34fd688dd9aeddf7
rnp0728 <rudra@sambuq.com>
2023-06-10 14:26:35 +0530

    Internship controller changes

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,5 +1,6 @@
 const Bookmark = require("../models/Bookmark");
 const Job = require("../models/Job");
+const Internship = require("../models/Internship");
 
 module.exports = {
     createBookmark: async (req, res) => {
@@ -41,4 +42,44 @@ module.exports = {
             res.status(500).json(error);
           }
     },
+
+    createInternshipBookmark: async (req, res) => {
+      const internship = req.body.internship;
+      try {
+        const internshipFound = await Internship.findById(internship);
+        if(!internshipFound) {
+          return res.status(404).json({error: "Job not Found"});
+        }
+        const newBookmark = new Bookmark({internship: internshipFound, userId: req.user.id});
+        console.log(newBookmark);
+          const savedBookmark = await newBookmark.save();
+          const {__v, updatedAt, ...newBookmarkInfo} = savedBookmark._doc;
+          res.status(201).json(newBookmarkInfo);
+
+      } catch (error) {
+          res.status(500).json(error);
+      }
+  },
+
+  deleteInternshipBookmark: async (req, res) => {
+      try {
+          const userId = req.user.id;
+          const internshipId = req.params.id;
+          await Bookmark.findOneAndDelete({userId, internshipId});
+    
+          res.status(200).json("Bookmark Deleted Successfully");
+        } catch (error) {
+          res.status(500).json(error);
+        }
+  },
+
+  getInternshipBookmarks: async (req, res) => {
+      try {
+          const bookmarks = await Bookmark.find({userId: req.user.id}).populate('internship');
+    
+          res.status(200).json(bookmarks);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+  },
 }
--- routes/bookmark.js
+++ routes/bookmark.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const bookmarkController = require("../controllers/bookmarkController");
-const {verifyAndAuthorize, verifyToken, verifyAdmin} = require("../middleware/verifyToken")
+const {verifyToken} = require("../middleware/verifyToken")
 
 
 // CREATE BOOKMARKS
@@ -14,6 +14,15 @@ router.delete("/:id", verifyToken, bookmarkController.deleteBookmark);
 // GET BOOKMARKS
 router.get("/", verifyToken, bookmarkController.getBookmarks);
 
+// CREATE INTERNSHIP BOOKMARKS
+router.post("/internships/", verifyToken,bookmarkController.createInternshipBookmark);
 
+
+// DELETE INTERNSHIP BOOKMARKS
+router.delete("/internships/:id", verifyToken, bookmarkController.deleteInternshipBookmark);
+
+
+// GET INTERNSHIP BOOKMARKS
+router.get("/internships/", verifyToken, bookmarkController.getInternshipBookmarks);
 
 module.exports = router
